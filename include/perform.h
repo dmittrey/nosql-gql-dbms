783c7a09276f56fdfc21eb58a5b4fe8f8c1f8f33
dmittrey <dmitryzubakhin@mail.ru>
2022-10-06 05:44:45 +0300

    Add interface to perform module

--- include/perform.h
+++ include/perform.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "query.h"
+
 /* 
 Abstraction to manipulate with data in RAM
 
@@ -7,3 +9,21 @@ Reponsibilities:
     - Handling queries and requests a file change
     - Work with requests
 */
+
+/* Polymorhism invokation file operations */
+
+#define TABLE_ACTION(data_type, op_type) TABLE_ACTION_##data_type_##op_type
+
+#define TABLE_ACTION_NODE_INSERT insertDocument
+#define TABLE_ACTION_NODE_READ   readDocument
+#define TABLE_ACTION_NODE_UPDATE updateDocument
+#define TABLE_ACTION_NODE_DELETE deleteDocument
+
+#define TABLE_ACTION_DOCUMENT_INSERT insertNode
+#define TABLE_ACTION_DOCUMENT_READ   readNode
+#define TABLE_ACTION_DOCUMENT_UPDATE updateNode
+#define TABLE_ACTION_DOCUMENT_DELETE deleteNode
+
+/* Perform queries */
+
+struct QueryResponse performAction(struct QueryRequest query_request);
