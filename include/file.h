a0f845196e6200fc497b7c856a8390aabe9baba3
dmittrey <dmitryzubakhin@mail.ru>
2022-10-06 05:43:42 +0300

    Add file interface

--- include/file.h
+++ include/file.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <stdio.h>
+
+#include "xml.h"
+#include "table.h"
+
 /* 
 Abstraction to manipulate with data representation in file
 
@@ -8,3 +13,50 @@ Reponsibilities:
     - Work with data and perform queries
     - Open and close file  
 */
+
+// Own program abstraction above file 
+struct TableFile {
+    FILE *file;
+};
+
+struct TransactionsResult {
+    struct TableFile *file; // Nullable or ptr to start queried data
+    enum PerformStatus status;
+};
+
+/* File manipulation */
+
+struct TransactionsResult createOrCreateTableStoreFile();
+
+struct TransactionsResult closeTableStoreFile();
+
+/* Section manipulation */
+
+enum PerformStatus createDocumentsSection(
+    struct TableFile *file,
+    struct Documents* prev_section // Can be null if section is first
+);
+
+enum PerformStatus createExtentSection(
+    struct TableFile *file,
+    struct Extent* prev_section // Can be null if section is first
+);
+
+/* Data manipulation */
+
+struct TransactionsResult insertDocument(struct TableFile *file, struct Document *document);
+
+struct TransactionsResult readDocument(struct TableFile *file, struct Document *document);
+
+struct TransactionsResult updateDocument(struct TableFile *file, struct Document *prev_document, struct Document *new_document);
+
+struct TransactionsResult deleteDocument(struct TableFile *file, struct Document *document);
+
+struct TransactionsResult insertData(struct TableFile *file, struct Node *node);
+
+struct TransactionsResult readData(struct TableFile *file, struct Node *node);
+
+struct TransactionsResult updateData(struct TableFile *file, struct Node *prev_node, struct Node *new_node);
+
+struct TransactionsResult deleteData(struct TableFile *file, struct Node *node);
+
