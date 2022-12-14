bf82c8cf943e6d8a06b2dfa81a7109205cd9c4c4
dmittrey <dmitryzubakhin@mail.ru>
2022-12-15 00:06:06 +0300

    Add page class decl

--- include/page.h
+++ include/page.h
@@ -1,16 +1,26 @@
 #pragma once
 
-#include "utils.h"
+#include "xml.h"
 
+/**
+ * Third layer of abstractions
+ *
+ * ----------------------
+ * | DB API             |
+ * ----------------------
+ * | DB FILE IO         |
+ * ----------------------
+ * | DB FILE SECTION IO |
+ * ----------------------
+ *
+ * @see include/table.h
+ */
 struct page_t;
 
 struct page_t *page_new();
 
-void page_ctor(struct page_t *);
-
+void page_ctor(struct page_t *, FILE *filep);
 void page_dtor(struct page_t *);
 
-PerformStatus page_insert(struct page_t *);
-PerformStatus page_update(struct page_t *);
-PerformStatus page_remove(struct page_t *);
-PerformStatus page_read(struct page_t *);
+PerformStatus page_write(struct page_t *, sectoff_t, node_t *);
+PerformStatus page_read(struct page_t *, sectoff_t, node_t *);
