9dc13857571d49d7afe7d99eb2514fe0d7b5ceee
dmittrey <dmitryzubakhin@mail.ru>
2022-12-15 21:34:38 +0300

    Add generic section  abstraction

--- include/section/section_page.h
+++ include/section/section_page.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include "utils.h"
+#include "table.h"
+
+#define SECTION_PAGE_FIELD_GETTER_DECLARE(FIELD_TYPE, FIELD_NAME) \
+    FIELD_TYPE FIELD_NAME section_page_get_##FIELD_NAME(section_page_t *page)
+
+#define SECTION_PAGE_FIELD_SETTER_DECLARE(FIELD_TYPE, FIELD_NAME) \
+    enum PerformStatus section_page_set_##FIELD_NAME(section_page_t *page, FIELD_TYPE FIELD_NAME)
 
 /**
  * Third layer of abstractions
@@ -19,8 +26,17 @@ struct section_page_t;
 
 struct section_page_t *section_page_new();
 
-void section_page_ctor(struct section_page_t *, FILE *filep);
+void section_page_ctor(struct section_page_t *, FILE *, fileoff_t);
 void section_page_dtor(struct section_page_t *);
 
-enum PerformStatus section_page_write(struct section_page_t *, sectoff_t, xml_node_t *);
-enum PerformStatus section_page_read(struct section_page_t *, sectoff_t, xml_node_t *);
+enum PerformStatus section_page_sync(section_page_t *);
+
+SECTION_PAGE_FIELD_GETTER_DECLARE(sectoff_t, free_space);
+SECTION_PAGE_FIELD_GETTER_DECLARE(sectoff_t, next);
+SECTION_PAGE_FIELD_GETTER_DECLARE(sectoff_t, last_item_ptr);
+SECTION_PAGE_FIELD_GETTER_DECLARE(sectoff_t, first_record_ptr);
+
+SECTION_PAGE_FIELD_SETTER_DECLARE(sectoff_t, free_space);
+SECTION_PAGE_FIELD_SETTER_DECLARE(sectoff_t, next);
+SECTION_PAGE_FIELD_SETTER_DECLARE(sectoff_t, last_item_ptr);
+SECTION_PAGE_FIELD_SETTER_DECLARE(sectoff_t, first_record_ptr);
---
+++ src/section_page.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#include "utils.h"
+#include "table.h"
+
+#define FWRITE_OR_FAIL(DATA_PTR, DATA_SIZE, FILP)  \
+    if (fwrite(DATA_PTR, DATA_SIZE, 1, FILP) != 1) \
+        return FAILED;
+
+#define SECTION_PAGE_FIELD_GETTER_DEFINE(FIELD_TYPE, FIELD_NAME)              \
+    FIELD_TYPE FIELD_NAME section_page_get_##FIELD_NAME(section_page_t *page) \
+    {                                                                         \
+        return page->FIELD_NAME;                                              \
+    }
+
+#define SECTION_PAGE_FIELD_SETTER_DEFINE(FIELD_TYPE, FIELD_NAME)                                  \
+    enum PerformStatus section_page_set_##FIELD_NAME(section_page_t *page, FIELD_TYPE FIELD_NAME) \
+    {                                                                                             \
+        page->FIELD_NAME = FIELD_NAME;                                                            \
+        FWRITE_OR_FAIL(&page->FIELD, sizeof(FIELD_TYPE), page);                                   \
+        return OK;                                                                                \
+    }
+
+typedef struct
+{
+    sectoff_t free_space;
+    sectoff_t next;             // Nullable
+    sectoff_t last_item_ptr;    // Pointer to first free cell(after items)
+    sectoff_t first_record_ptr; // Pointer to last free cell(before records)
+    fileoff_t fileoff;
+    FILE *filp;
+} section_page_t;
+
+struct section_page_t *section_page_new()
+{
+    return my_malloc(section_page_t);
+}
+
+void section_page_ctor(section_page_t *page, FILE *filp, fileoff_t fileoff)
+{
+    page->free_space = SECTION_SIZE - sizeof(section_page_t);
+    page->next = NULL;
+    page->last_item_ptr = sizeof(section_page_t);
+    page->first_record_ptr = SECTION_SIZE - 1;
+    page->fileoff = fileoff;
+    page->filp = filp;
+}
+void section_page_dtor(section_page_t *page)
+{
+    free(page);
+}
+
+/**
+ * Write inner information into file
+ */
+enum PerformStatus section_page_sync(section_page_t *page)
+{
+    FWRITE_OR_FAIL(page->free_space, sizeof(page->free_space), page->filp);
+    FWRITE_OR_FAIL(page->next, sizeof(page->next), page->filp);
+    FWRITE_OR_FAIL(page->last_item_ptr, sizeof(page->last_item_ptr), page->filp);
+    FWRITE_OR_FAIL(page->first_record_ptr, sizeof(page->first_record_ptr), page->filp);
+
+    return OK;
+}
+
+SECTION_PAGE_FIELD_GETTER_DEFINE(sectoff_t, free_space);
+SECTION_PAGE_FIELD_GETTER_DEFINE(sectoff_t, next);
+SECTION_PAGE_FIELD_GETTER_DEFINE(sectoff_t, last_item_ptr);
+SECTION_PAGE_FIELD_GETTER_DEFINE(sectoff_t, first_record_ptr);
+
+SECTION_PAGE_FIELD_SETTER_DEFINE(sectoff_t, free_space);
+SECTION_PAGE_FIELD_SETTER_DEFINE(sectoff_t, next);
+SECTION_PAGE_FIELD_SETTER_DEFINE(sectoff_t, last_item_ptr);
+SECTION_PAGE_FIELD_SETTER_DEFINE(sectoff_t, first_record_ptr);
