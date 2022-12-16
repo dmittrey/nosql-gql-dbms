9198e3bfa37550fb824461844e2be596313bf52c
dmittrey <dmitryzubakhin@mail.ru>
2022-12-16 05:27:58 +0300

    Change documents section API

--- include/section/section_documents.h
+++ include/section/section_documents.h
@@ -8,10 +8,14 @@
 
 struct section_documents_t;
 
-struct section_documents_t *page_new();
+struct section_documents_t *section_documents_new();
 
-void page_ctor(struct section_documents_t *, FILE *filep);
-void page_dtor(struct section_documents_t *);
+void section_documents_ctor(struct section_documents_t *, FILE *, fileoff_t);
+void section_documents_dtor(struct section_documents_t *);
 
-enum PerformStatus section_documents_write(struct section_documents_t *, struct xml_document_t *);
+enum PerformStatus section_documents_sync(struct section_documents_t *);
+
+enum PerformStatus section_documents_insert(struct section_documents_t *, struct xml_document_t *);
 enum PerformStatus section_documents_read(struct section_documents_t *, struct xml_document_t *);
+enum PerformStatus section_documents_update(struct section_documents_t *, struct xml_document_t *);
+enum PerformStatus section_documents_delete(struct section_documents_t *, struct xml_document_t *);
