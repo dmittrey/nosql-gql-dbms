2a91177c72e2ceba39c63d50f9b5bb10918d6d91
dmittrey <dmitryzubakhin@mail.ru>
2022-12-16 05:29:00 +0300

    Add methods to xml document to get name attr's

--- include/xml/xml_document.h
+++ include/xml/xml_document.h
@@ -10,5 +10,10 @@ struct xml_document_t;
 
 struct xml_document_t *xml_document_new();
 
-void xml_document_ctor(struct xml_document_t *document, string_t *name, struct xml_node_t *root);
+void xml_document_ctor(struct xml_document_t *, string_t *, struct xml_document_t *);
 void xml_document_dtor(struct xml_document_t *);
+
+int xml_document_get_name_count(struct xml_document_t *);
+char *xml_document_get_name_val(struct xml_document_t *);
+
+int xml_document_get_name_size(struct xml_document_t *);
