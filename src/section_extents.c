ac2e647f20fe82a8159434e3ecf15330023f9745
dmittrey <dmitryzubakhin@mail.ru>
2023-02-11 05:21:06 +0300

    Add extent section insert operation

--- src/section_extents.c
+++ src/section_extents.c
@@ -4,6 +4,10 @@
 #include "section/section_header.h"
 #include "section/section_extents.h"
 
+#include "entity/json_value_entity.h"
+
+#define ITEM_SIZE 8
+
 static PerformStatus section_extents_write_in_item(section_extents_t *, size_t, void *);
 static PerformStatus section_extents_write_in_record(section_extents_t *, size_t, void *, fileoff_t *);
 
@@ -26,39 +30,14 @@ PerformStatus section_extents_sync(section_extents_t *section)
     return section_page_sync((section_header_t *)section);
 }
 
-/*
-1) Сериализовать согласно физическому представлению
-2) Записать Items
-3) Записать Records
-*/
-PerformStatus section_extents_write(section_extents_t *section, json_value_t *json, fileoff_t *save_addr)
+PerformStatus section_extents_write(section_extents_t *section, json_value_t *json, fileoff_t *parent_json_addr, fileoff_t *save_addr)
 {
     if (section->header.free_space <= json_value_get_item_size(json) + json_value_get_record_size(json))
     {
-        long prev_ptr = ftell(section->header.filp);
-        FSEEK_OR_FAIL(section->header.filp, section->header.last_item_ptr);
+        fileoff_t json_val_ptr = 0;
+
         *save_addr = section->header.last_item_ptr;
 
-        FWRITE_OR_FAIL(&json->object.attributes_count, sizeof(uint64_t), section->header.filp);
-        for (size_t i = 0; i < json->object.attributes_count; i++)
-        {
-            // Write key length
-            section_extents_write_in_item(section, sizeof(uint64_t), &json->object.attributes[i]->key.count);
-
-            // Write key value and ptr
-            fileoff_t key_val_ptr = 0;
-            fileoff_t obj_val_ptr = 0;
-            section_extents_write_in_record(section, string_get_size(json->object.attributes[i]->key), json->object.attributes[i]->key.val, &key_val_ptr);
-            section_extents_write(section, json->object.attributes[i]->value, &obj_val_ptr);
-
-            // Write key name ptr and value ptr
-            section_extents_write_in_item(section, sizeof(uint64_t), key_val_ptr);
-            section_extents_write_in_item(section, sizeof(uint64_t), obj_val_ptr);
-        }
-
-        FWRITE_OR_FAIL(&json->type, sizeof(uint64_t), section->header.filp);
-
-        fileoff_t json_val_ptr = 0;
         if (json->type == TYPE_STRING)
         {
             section_extents_write_in_record(section, string_get_size(json->value.string_val), json->value.string_val.val, &json_val_ptr);
@@ -67,14 +46,41 @@ PerformStatus section_extents_write(section_extents_t *section, json_value_t *js
         {
             section_extents_write_in_record(section, sizeof(json->value), &json->value, &json_val_ptr);
         }
-        section_extents_write_in_item(section, sizeof(uint64_t), json_val_ptr);
+
+        section_extents_write_in_item(section, ITEM_SIZE, &json->object.attributes_count);
+        section_extents_write_in_item(section, ITEM_SIZE, &json->type);
+        section_extents_write_in_item(section, ITEM_SIZE, &json_val_ptr);
+        section_extents_write_in_item(section, ITEM_SIZE, parent_json_addr);
+        section_extents_write_in_item(section, ITEM_SIZE, 0); // Hold gap for next json addr
+
+        for (size_t i = 0; i < json->object.attributes_count; i++)
+        {
+            sectoff_t attr_key_ptr = 0;
+            sectoff_t attr_val_ptr = 0;
+            
+            kv *cur_attribute = json->object.attributes[i];
+
+            // Write attr key, save ptr and size into entity
+            section_extents_write_in_record(section, string_get_size(cur_attribute->key), cur_attribute->key.val, &attr_key_ptr);
+            uint64_t attr_key_size = cur_attribute->key.count;
+
+            // Write attr value, save ptr(We can fetch size via type)
+            section_extents_write(section, json->object.attributes[i]->value, save_addr, &attr_val_ptr);
+
+            // Save items of attribute(key_size, key_ptr, val_ptr)
+            section_extents_write_in_item(section, ITEM_SIZE, &cur_attribute->key.count);
+            section_extents_write_in_item(section, ITEM_SIZE, &attr_key_ptr);
+            section_extents_write_in_item(section, ITEM_SIZE, &attr_val_ptr);
+        }
 
         return OK;
     }
 
     return FAILED;
 }
-PerformStatus section_documents_read(section_extents_t *, json_value_t *);
+PerformStatus section_documents_read(section_extents_t *section, json_value_t *json)
+{
+}
 PerformStatus section_documents_update(section_extents_t *, json_value_t *);
 PerformStatus section_documents_delete(section_extents_t *, json_value_t *);
 
