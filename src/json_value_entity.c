713121eb5aaac0185a249f6e79a59f71da488431
dmittrey <dmitryzubakhin@mail.ru>
2023-02-11 05:20:42 +0300

    Implmement json_value ops

---
+++ src/json_value_entity.c
@@ -0,0 +1,32 @@
+#include "entity/json_value_entity.h"
+
+json_value_entity *json_value_entity_new()
+{
+    return my_malloc(json_value_entity);
+}
+
+void json_value_entity_ctor(json_value_entity *json_entity, json_value_t *json)
+{
+    json_entity->attr_count = json->object.attributes_count;
+    json_entity->type = json->type;
+    json_entity->val_ptr = 0;
+    json_entity->parent = 0;
+    json_entity->next = 0;
+    json_entity->attributes = my_malloc_array(kv_entity, json_entity->attr_count);
+    return json_entity;
+}
+void json_value_entity_dtor(json_value_entity *json_entity)
+{
+    free(json_entity->attributes);
+    free(json_entity);
+}
+
+size_t json_value_entity_get_size(json_value_entity *json)
+{
+    return sizeof(json->attr_count) +
+           sizeof(json->type) +
+           sizeof(json->val_ptr) +
+           sizeof(json->parent) +
+           sizeof(json->next) +
+           json->attr_count * sizeof(kv_entity);
+}
