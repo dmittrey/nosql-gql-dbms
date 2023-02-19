c825409d2bb27e06ea3fe7088ee4de21136de671
dmittrey <dmitryzubakhin@mail.ru>
2023-02-19 20:24:44 +0000

    Change string live cycle methods

--- solution/src/utils.c
+++ solution/src/utils.c
@@ -2,9 +2,23 @@
 
 #include "utils.h"
 
-string_t string_ctor(const char *string, size_t count)
+void string_new(string_t *string)
 {
-    return (string_t) {.count = count, .val = string};
+    string = my_malloc(string_t);
+}
+
+void string_ctor(string_t *const string, const char *const value, const size_t count)
+{
+    char *value = my_malloc_array(char, count);
+
+    string->val = strncpy(value, string, count);
+    string->count = count;
+}
+
+void string_dtor(string_t *string)
+{
+    free(string->val);
+    free(string);
 }
 
 size_t string_get_size(string_t str)
