fec2ac02651717739e217a3a5b470af701802f59
dmittrey <dmitryzubakhin@mail.ru>
2023-02-27 02:47:34 +0300

    Change file api

--- solution/include/memory/file/file.h
+++ solution/include/memory/file/file.h
@@ -12,9 +12,9 @@ file_t *file_new();
 void file_ctor(file_t *const, FILE *const);
 status_t file_dtor(file_t *);
 
-status_t file_write(file_t *const file, const json_t *const json);
-status_t file_read(file_t *const file, const char *path, json_t *const ret_json);
-status_t file_update(file_t *const file, const char *path, const json_t *const new_json);
-status_t file_delete(file_t *const file, const char *path);
+status_t file_write(file_t *const file, const json_t *const json, const fileoff_t* write_addr);
+status_t file_read(file_t *const file, const fileoff_t fileoff, json_t *const ret_json);
+status_t file_update(file_t *const file, const fileoff_t fileoff, const json_t *const new_json);
+status_t file_delete(file_t *const file, const fileoff_t fileoff);
 
 sect_ext_t *get_sect_ext(fileoff_t);
