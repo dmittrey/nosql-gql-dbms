8b62c866d8efa49df2a0d41590d687bcec8e918e
dmittrey <dmitryzubakhin@mail.ru>
2023-02-28 01:28:27 +0300

    Declare test function cases

--- solution/src/main.c
+++ solution/src/main.c
@@ -2,13 +2,21 @@ void test_extents_write();
 void test_extents_read();
 void test_extents_update();
 void test_extents_delete();
+
+void test_file_write();
+void test_file_read();
+
 void test_header();
 
 int main(int argc, char **argv)
 {
     test_header();
+
     test_extents_write();
     test_extents_read();
     test_extents_update();
     test_extents_delete();
+
+    test_file_write();
+    test_file_read();
 }
