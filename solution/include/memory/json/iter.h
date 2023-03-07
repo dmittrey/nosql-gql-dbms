11c2d4d26d5862b4e5b0d3dfd3265e1b2c69254d
dmittrey <dmitryzubakhin@mail.ru>
2023-03-07 03:52:39 +0300

    Fix iter constraints

--- solution/include/memory/json/iter.h
+++ solution/include/memory/json/iter.h
@@ -13,7 +13,7 @@ typedef struct
 
 iter_t *iter_new();
 
-void iter_ctor(iter_t *const, const file_t *const, const query_t *const);
+void iter_ctor(iter_t *const, file_t *, query_t *);
 void iter_dtor(iter_t *);
 
 status_t iter_next(iter_t *const);
