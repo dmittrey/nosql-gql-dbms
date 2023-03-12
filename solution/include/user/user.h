71b5457572bb34b630361c86d607e768e992d05e
dmittrey <dmitryzubakhin@mail.ru>
2023-03-12 07:00:16 +0000

    Complete user api interface

--- solution/include/user/user.h
+++ solution/include/user/user.h
@@ -4,6 +4,12 @@
 
 #include "memory/json/iter.h"
 
+#include "memory/type/type.h"
+
+status_t user_json_type_add(file_t *const file, const type_t* const type);
+status_t user_json_type_delete(file_t *const file, const string_t *const name);
+status_t user_json_type_find(file_t *const file, const string_t *const name, type_t* const o_type);
+
 status_t user_write(file_t *const file, const json_t *const json);
 iter_t *user_read(file_t *const file, const query_t *const query);
 status_t user_update(file_t *const file, const query_t *const query, const json_t *const new_json);
