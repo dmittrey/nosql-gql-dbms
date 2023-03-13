454b26fb638f482e4fdb33a11a4b0fc9f52709c5
dmittrey <dmitryzubakhin@mail.ru>
2023-03-13 18:42:56 +0300

    Done impl user api logic

--- solution/src/user/user.c
+++ solution/src/user/user.c
@@ -10,7 +10,7 @@ status_t user_delete_type(file_t *const file, const string_t *const name)
 }
 status_t user_find_type(file_t *const file, const string_t *const name, type_t *const o_type)
 {
-    sect_type_t* del_types = NULL;
+    sect_type_t *del_types = NULL;
     return file_find_type(file, name, o_type, &del_types);
 }
 
@@ -19,30 +19,38 @@ status_t user_write(file_t *const file, const json_t *const json)
     fileoff_t wrt_adr;
     return file_write(file, json, 0, &wrt_adr);
 }
-iter_t *user_read(file_t *const file, query_t *const query)
+struct iter_t *user_read(file_t *const file, query_t *const query)
 {
-    iter_t *iter = iter_new();
+    struct iter_t *iter = iter_new();
     iter_ctor(iter, file, query);
 
     return iter;
 }
 status_t user_update(file_t *const file, query_t *const query, const json_t *const new_json)
 {
-    iter_t *iter = iter_new();
+    struct iter_t *iter = iter_new();
     iter_ctor(iter, file, query);
 
-    // Iterate over json
-    //  And delete in fileoff(json field)
+    fileoff_t upd_fileoff;
+    while (iter_is_avail(iter))
+    {
+        file_update(file, iter_get(iter)->foff, new_json, 0, false, &upd_fileoff);
+        iter_next(iter);
+    }
 
     return OK;
 }
 status_t user_delete(file_t *const file, query_t *const query)
 {
-    iter_t *iter = iter_new();
+    struct iter_t *iter = iter_new();
     iter_ctor(iter, file, query);
 
-    // Iterate over json
-    //  And delete in fileoff(json field)
+    fileoff_t upd_fileoff;
+    while (iter_is_avail(iter))
+    {
+        file_delete(file, iter_get(iter)->foff, true);
+        iter_next(iter);
+    }
 
     return OK;
 }
