5d8a101afcef9bf7e982eedaf8038989c4b61878
dmittrey <dmitryzubakhin@mail.ru>
2023-04-01 05:00:43 +0300

    Ping pong done

--- grpc/dbms_server.cc
+++ grpc/dbms_server.cc
@@ -2,8 +2,6 @@
 #include <memory>
 #include <string>
 
-#include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
 #include "absl/strings/str_format.h"
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
@@ -20,44 +18,44 @@ using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
 
-ABSL_FLAG(uint16_t, port, 50051, "Server port for the service");
-
 class DataBaseServiceImpl final : public DataBase::Service
 {
-    Status Ping(ServerContext *context, const HelloRequest *request,
-                HelloResponse *response) override
+private:
+    std::unique_ptr<Server> server_;
+
+    DataBaseServiceImpl() : DataBase::Service() {}
+
+public:
+    DataBaseServiceImpl(uint16_t port) : DataBaseServiceImpl("localhost", port) {}
+
+    DataBaseServiceImpl(std::string address, uint16_t port)
     {
-        std::string prefix = {"Hello "};
-        response->set_message(true);
+        ServerBuilder builder;
+
+        DataBaseServiceImpl service{};
+
+        std::string server_address = address + ":" + std::to_string(port);
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+        builder.RegisterService(&service);
+
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "Server listening on " << server_address << std::endl;
+
+        server->Wait();
+    }
+
+    Status Ping(ServerContext *context, const HelloRequest *request, HelloResponse *response) override
+    {
+        std::cout << "Received from client: " << request->message() << std::endl;
+        response->set_message("Pong");
         return Status::OK;
     }
 };
 
-void RunServer(uint16_t port)
-{
-    std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
-    DataBaseServiceImpl service{};
-
-    grpc::EnableDefaultHealthCheckService(true);
-    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-    ServerBuilder builder;
-    // Listen on the given address without any authentication mechanism.
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    // Register "service" as the instance through which we'll communicate with
-    // clients. In this case it corresponds to an *synchronous* service.
-    builder.RegisterService(&service);
-    // Finally assemble the server.
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on " << server_address << std::endl;
-
-    // Wait for the server to shutdown. Note that some other thread must be
-    // responsible for shutting down the server for this call to ever return.
-    server->Wait();
-}
-
 int main(int argc, char **argv)
 {
-    absl::ParseCommandLine(argc, argv);
-    RunServer(absl::GetFlag(FLAGS_port));
+    DataBaseServiceImpl(5433);
+
     return 0;
 }
