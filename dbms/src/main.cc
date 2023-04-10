facce354a51bff3d39942d0cabe24bd042584bdb
dmittrey <dmitryzubakhin@mail.ru>
2023-04-10 04:05:21 +0300

    Add server's port and address selection

--- dbms/src/main.cc
+++ dbms/src/main.cc
@@ -3,15 +3,23 @@
 
 extern "C"
 {
-    #include "tests/test.h"
+#include "tests/test.h"
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
     do_tests();
 
     Driver driver;
-    DataBaseServiceImpl server{driver, 5433};
+    DataBaseServiceImpl *server;
+    if (argc == 1)
+        server = new DataBaseServiceImpl(driver, 5432);
+    if (argc == 2)
+        server = new DataBaseServiceImpl(driver, std::stoi(argv[1]));
+    else if (argc == 3)
+        server = new DataBaseServiceImpl(driver, std::stoi(argv[1]), argv[2]);
+    else
+        return 1;
 
     return 0;
 }
